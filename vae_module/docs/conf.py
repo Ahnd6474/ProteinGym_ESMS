1c1dff06996b48d6993873fd574e7078ab4439b0
Danny <ahnd6474@gmail.com>
2025-06-29 23:16:46 +0900

    Delete vae_module directory

--- vae_module/docs/conf.py
+++
@@ -1,4 +0,0 @@
-project = 'vae_module'
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
-exclude_patterns = ['_build']
-html_theme = 'alabaster'
--- vae_module/docs/index.rst
+++
@@ -1,8 +0,0 @@
-vae_module Documentation
-=======================
-
-.. toctree::
-   :maxdepth: 2
-   :caption: Contents:
-
-   api
